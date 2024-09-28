842bb24b64d1aaab72d54b02c8d34ee23ded4b3d
EasySouls <tarjanyicsanad@gmail.com>
2024-09-28 11:49:46 +0200

    Added navbar

---
+++ src/components/common/navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import NavItem from "./nav-item";
+
+const navItems = [
+  { href: "/renting", title: "Bérlés" },
+  { href: "/about", title: "Rólunk" },
+];
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between px-4 py-2">
+      <Link href="/">
+        <h1>Edénykölcsönző</h1>
+      </Link>
+      <ul className="flex gap-2">
+        {navItems.map((item) => (
+          <li key={item.href}>
+            <NavItem href={item.href} title={item.title} />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
